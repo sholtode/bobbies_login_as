86499ed4863dbfff436bfcaa5a08e94228abd4d0
Sholto Douglas <sholto-douglas@ms-systems.eu>
2022-10-19 14:21:37 +0200

    chore: add MS Systems to manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # (C) 2021 Bobbies (<https://www.bobbies.com>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+# add branch 15.0 for Odoo 15 - 2022 MS Systems e.K. (<https://mw-systems.eu>)
 
 {
     'name': 'Login as another user',
-    'version': '0.2',
+    'version': '15.0.1.0.0',
     'license': 'LGPL-3',
     'category': 'Tools',
     'author': 'Bobbies',
